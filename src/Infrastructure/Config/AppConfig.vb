a75f96dda4849be0e282ce5b585170b4341ac3db
A. Jalal <195860688+AhmedGalalIsmail@users.noreply.github.com>
2026-06-16 13:56:15 +0300

    Update AppConfig.vb

--- src/Infrastructure/Config/AppConfig.vb
+++ src/Infrastructure/Config/AppConfig.vb
@@ -5,6 +5,9 @@ Imports System.IO
 Imports Infrastructure.Logging
 
 Namespace Config
+    ''' <summary>
+    ''' 
+    ''' </summary>
     Public Class AppConfig
         Public Property AppName As String = "CoNSoL-TakeOff"
         Public Property LogDir As String = "logs"
@@ -18,11 +21,16 @@ Namespace Config
         ''' </summary>
         ''' <param name="path"></param>
         ''' <param name="logger"></param>
-        ''' <returns></returns>
         Public Function Load(path As String, logger As ILogger) As AppConfig
             Return Load(path, logger, _logger)
         End Function
 
+        ''' <summary>
+        ''' Loads configuration from specified path. 
+        ''' </summary>
+        ''' <param name="path"></param>
+        ''' <param name="logger"></param>
+        ''' <param name="_logger"></param>
         Public Shared Function Load(path As String, logger As ILogger, _logger As ILogger) As AppConfig
             _logger = logger
             If Not File.Exists(path) Then
@@ -34,6 +42,10 @@ Namespace Config
             Return JsonSerializer.Deserialize(Of AppConfig)(json)
         End Function
 
+        ''' <summary>
+        ''' Loads configuration from specified path.
+        ''' </summary>
+        ''' <param name="path"></param>
         Public Shared Function Load(path As String) As AppConfig
             If Not File.Exists(path) Then
                 Dim def = New AppConfig()
@@ -43,17 +55,5 @@ Namespace Config
             Dim json = File.ReadAllText(path)
             Return JsonSerializer.Deserialize(Of AppConfig)(json)
         End Function
-
-        Public Function LoadFromFile(configPath As String) As AppConfig
-            _logger.Info($"Loading config from: {configPath}")
-            Try
-                ' Load logic
-                _logger.Info("Config loaded successfully")
-            Catch ex As Exception
-                _logger.[Error]($"Config load failed: {ex.Message}")
-                Throw
-            End Try
-        End Function
-
     End Class
 End Namespace
