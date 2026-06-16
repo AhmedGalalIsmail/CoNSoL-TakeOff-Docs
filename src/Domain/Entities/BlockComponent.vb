19f1d406362b9804edc8f3037b904864bbf7420f
A. Jalal <195860688+AhmedGalalIsmail@users.noreply.github.com>
2026-06-16 14:06:35 +0300

    Update BlockComponent.vb

--- src/Domain/Entities/BlockComponent.vb
+++ src/Domain/Entities/BlockComponent.vb
@@ -1,11 +1,18 @@
-﻿
+
 Namespace Entities
-    ''' <summary>
-    ''' Represents a single component of a block, 
-    ''' linking to a material and specifying quantity per unit of block.
-    ''' </summary>
-    Public Class BlockComponent
-        Public Property MaterialCode As String
-        Public Property QuantityPerUnit As Decimal
-    End Class
+	''' <summary>
+	''' Represents a single component of a block, 
+	''' linking to a material and specifying quantity per unit of block.
+	''' </summary>
+	Public Class BlockComponent
+		''' <summary>
+		''' The code of the material associated with this block component.
+		''' </summary>
+		Public Property MaterialCode As String
+
+		''' <summary>
+		''' The quantity of the material required per unit of the block.
+		''' </summary>
+		Public Property QuantityPerUnit As Decimal
+	End Class
 End Namespace
