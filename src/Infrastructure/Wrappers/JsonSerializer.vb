53008c919495b90246866bfd666582253acd4eb5
A. Jalal <195860688+AhmedGalalIsmail@users.noreply.github.com>
2026-06-16 08:20:23 +0300

    Add XML documentation to JsonSerializerWrapper
    Added XML documentation comments to the JsonSerializerWrapper class and its methods for better clarity and maintainability.

--- src/Infrastructure/Wrappers/JsonSerializer.vb
+++ src/Infrastructure/Wrappers/JsonSerializer.vb
@@ -1,11 +1,31 @@
-﻿Option Strict On
+Option Strict On
 Imports System.Text.Json
 
 Namespace Wrappers
+    ''' <summary>
+    ''' A simple wrapper around System.Text.Json to centralize JSON serialization logic.
+    ''' </summary>
     Public Class JsonSerializerWrapper
+        ''' <summary>
+        ''' Serializes an object to a JSON string using System.Text.Json.
+        ''' </summary>
+        ''' <typeparam name="T"></typeparam>
+        ''' <param name="obj"></param>
+        ''' <returns>
+        ''' A JSON string representation of the object.
+        ''' </returns>
         Public Shared Function Serialize(Of T)(obj As T) As String
             Return JsonSerializer.Serialize(obj)
         End Function
+
+        ''' <summary>
+        ''' Deserializes a JSON string to an object of type T using System.Text.Json.
+        ''' </summary>
+        ''' <typeparam name="T"></typeparam>
+        ''' <param name="json"></param>
+        ''' <returns>
+        ''' An object of type T deserialized from the JSON string.
+        ''' </returns>
         Public Shared Function Deserialize(Of T)(json As String) As T
             Return JsonSerializer.Deserialize(Of T)(json)
         End Function
